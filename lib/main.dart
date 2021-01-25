53ce3178eb035584eeee8e0d5587e1d18cdcbcd3
guopeng li <guopeng.li@technicolor.com>
2021-01-25 09:29:14 +0100

    refactor data to a separate class and set data to private

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:quizzler/question.dart';
+import 'package:quizzler/quiz_brain.dart';
+
+QuizBrain quizBrain = new QuizBrain();
 
 void main() => runApp(Quizzler());
 
@@ -28,14 +30,6 @@ class QuizPage extends StatefulWidget {
 class _QuizPageState extends State<QuizPage> {
   List<Icon> scoreKeeper = [];
 
-  List<Question> questions = [
-    Question(q: 'You can lead a cow down stairs but not up stairs.', a: false),
-    Question(
-        q: 'Approximately one quarter of human bones are in the feet.',
-        a: true),
-    Question(q: 'A slug\'s blood is green.', a: true)
-  ];
-
   int questionNumber = 0;
   @override
   Widget build(BuildContext context) {
@@ -49,7 +43,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                questions[questionNumber].questionText,
+                quizBrain.getQuestionText(questionNumber),
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -74,12 +68,12 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 setState(() {
-                  questionNumber++;
-                  if (questions[questionNumber].questionAnswer) {
+                  if (quizBrain.getQuestionAnswer(questionNumber) == true) {
                     scoreKeeper.add(Icon(Icons.check, color: Colors.green));
                   } else {
                     scoreKeeper.add(Icon(Icons.close, color: Colors.red));
                   }
+                  questionNumber++;
                 });
               },
             ),
@@ -99,12 +93,12 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 setState(() {
-                  questionNumber++;
-                  if (!questions[questionNumber].questionAnswer) {
+                  if (quizBrain.getQuestionAnswer(questionNumber) == false) {
                     scoreKeeper.add(Icon(Icons.check, color: Colors.green));
                   } else {
                     scoreKeeper.add(Icon(Icons.close, color: Colors.red));
                   }
+                  questionNumber++;
                 });
               },
             ),
